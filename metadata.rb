873785a3bf467b6f3900206cfdb09e2e06dbbcf0
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-15 14:05:47 +0100

    Release 1.0.4: fix a bug that could prevent proper installation from the PPA repo.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Andrea Campi"
 maintainer_email  "andrea.campi@zephirworks.com"
 license           "Apache 2.0"
 description       "Installs and configures nginx for CloudFoundry"
-version           "1.0.3"
+version           "1.0.4"
 
 %w{ ubuntu }.each do |os|
   supports os
